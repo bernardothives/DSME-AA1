29a4e7458c2f912f86e7d101539f9c350a62fbfc
bernardothives <bernardoalvesthives@gmail.com>
2025-04-09 13:57:58 -0300

    add images

--- constants/Stores.ts
+++ constants/Stores.ts
@@ -4,8 +4,8 @@ export const stores = [
       "name": "Burger Master",
       "banner": "https://images.unsplash.com/photo-1625331725309-83e4f3c1373b?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "photos": [
-        "@/assets/images/hamburger.jpg",
-        "https://images.unsplash.com/photo-1543163521-1bf6f311c653"  // URL direto obtido no botão download do Unsplash
+        "https://invexo.com.br/blog/wp-content/uploads/2021/06/Hamburgueria-da-Alf%C3%A2ndega-Ipanema.jpg",
+        "https://www.burgerfoods.com.br/wp-content/uploads/2022/12/front-view-male-holding-tray-with-burger-and-fries_Easy-Resize.com_.jpg"  
       ],
       "description": "Os hambúrgueres mais suculentos e artesanais da cidade.",
       "phone": "+550011112222",
@@ -20,8 +20,8 @@ export const stores = [
       "name": "Pizza da Mamma",
       "banner": "https://images.unsplash.com/photo-1513104890138-7c749659a591?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink",
       "photos": [
-        "https://example.com/pizza-1.jpg",
-        "https://example.com/pizza-2.jpg"
+        "https://revistashoppingcenters.com.br/wp-content/uploads/2019/12/abrasce_capa_PizzaMakers_inova_no_conceito_de_pizzaria.jpg",
+        "https://napolepizzaria.com.br/assets/images/pizzaria-poos-de-caldas-whatsapp-1076x718.jpg"
       ],
       "description": "Tradição italiana com um toque brasileiro.",
       "phone": "+550022223333",
@@ -37,7 +37,8 @@ export const stores = [
         "name": "Sushi Zen",
         "banner": "https://images.unsplash.com/photo-1613914359753-fda82bfc2a22?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         "photos": [
-            "https://example.com/sushi-1.jpg"
+            "https://images.squarespace-cdn.com/content/v1/63fc9d916fb0b31ab06c0ead/34ff468e-3358-4e3f-ac4d-9d9b717e29bb/restaurante-japones-morumbi.jpg",
+            "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/bb/84/71/o-melhor-restaurante.jpg?w=600&h=-1&s=1"
         ],
         "description": "Experiência japonesa completa com qualidade e sabor.",
         "phone": "+550033334444",
@@ -52,7 +53,8 @@ export const stores = [
         "name": "Chopp & Cia",
         "banner": "https://images.unsplash.com/photo-1441985969846-3e7c90531139?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
         "photos": [
-            "https://example.com/chopp-1.jpg"
+            "https://www.baressp.com.br/bares/fotos/chopperia_liberdade01.jpg",
+            "https://feiradosprodutores.com.br/wp-content/uploads/2018/11/trem-na-chapa-600x400.jpg"
         ],
         "description": "A melhor seleção de chopes artesanais e petiscos.",
         "phone": "+550044445555",
@@ -67,8 +69,8 @@ export const stores = [
         "name": "Doçuras da Vó",
         "banner": "https://plus.unsplash.com/premium_photo-1663127434986-ebb292c45460?q=80&w=2038&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         "photos": [
-            "https://example.com/doces-1.jpg",
-            "https://example.com/doces-2.jpg"
+            "https://confetdoceria.com.br/wp-content/uploads/2023/02/IMG_0339-scaled.jpg",
+            "https://eduk.contentools.com/api/v1/media/260422/download/"
         ],
         "description": "Doces caseiros com aquele gostinho de infância.",
         "phone": "+550055556666",
